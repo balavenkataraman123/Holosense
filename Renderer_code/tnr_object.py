f494356b8eb48e7d64e13e17d60d20418f59352b
Bala Venkataraman <38150148+balavenkataraman123@users.noreply.github.com>
2024-06-02 20:53:55 -0400

    Update tnr_object.py

--- Renderer_code/tnr_object.py
+++ Renderer_code/tnr_object.py
@@ -30,16 +30,15 @@ HFOV = thisconf.HFOV * math.pi / 180
 VFOV = thisconf.VFOV * math.pi / 180
 htan = - math.tan(HFOV/2)
 vtan =  math.tan(VFOV/2)
+
 dispw = thisconf.dispw
 disph = thisconf.disph
 screen_diagonal = thisconf.screen_diagonal # measurement is in inches
 camera_y_offset = thisconf.camera_y_offset # vertical distance between the center of the screen and the center of the camera lens in inches
 camera_x_offset = thisconf.camera_x_offset
 camera_z_offset = thisconf.camera_z_offset
-scale = 8/dispw
-killed = 0
-endist = 3
-eedist = 4
+endist = thisconf.endist
+eedist = thisconf.eedist
 
 cap = cv2.VideoCapture(thisconf.cameraname)
 thisconf.configcamera(thisconf, cap)
@@ -47,6 +46,7 @@ inference_interval = thisconf.inference_interval
 lfp = thisconf.log_file
 logs = []
 starttime = round(time.time() * 1000)
+
 
 
 def camerastuff(): # VIDEO ANALYSIS - GETS FACIAL COORDINATES AND DOES SPICY MATH TO FIND SPATIAL POSITION OF POV
@@ -86,7 +86,7 @@ def camerastuff(): # VIDEO ANALYSIS - GETS FACIAL COORDINATES AND DOES SPICY MAT
         refine_landmarks=True,
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5) as face_mesh:
-        while killed == 0:
+        while True:
             success, image = cap.read()
             if not success:
                 print("Ignoring empty camera frame.")
@@ -118,12 +118,12 @@ def camerastuff(): # VIDEO ANALYSIS - GETS FACIAL COORDINATES AND DOES SPICY MAT
                     cv2.circle(image, (int(nx), int(ny)), 3, (255, 0, 0), -1)
 
             
-            e1h = htan * ((camw/2) - rex)/640 
-            e1v = vtan * ((camh/2) - rey)/360
-            e2h = htan * ((camw/2) - lex)/640
-            e2v = vtan * ((camh/2) - ley)/360
-            nh = htan * ((camw/2) - nx)/640
-            nv = vtan * ((camh/2) - ny)/360
+            e1h = htan * ((camw/2) - rex)/(camw/2) 
+            e1v = vtan * ((camh/2) - rey)/(camh/2)
+            e2h = htan * ((camw/2) - lex)/(camw/2)
+            e2v = vtan * ((camh/2) - ley)/(camh/2)
+            nh = htan * ((camw/2) - nx)/(camw/2)
+            nv = vtan * ((camh/2) - ny)/(camh/2)
             # angle cosine calculation by dot products 
             eec = ((e1h * e2h) + (e1v * e2v) + 1) / (((e1h ** 2 + e1v ** 2 + 1) ** 0.5) * ((e2h ** 2 + e2v ** 2 + 1) ** 0.5))
             en1 = ((e1h * nh) + (e1v * nv) + 1) / (((e1h ** 2 + e1v ** 2 + 1) ** 0.5) * ((nh ** 2 + nv ** 2 + 1) ** 0.5))
@@ -132,7 +132,6 @@ def camerastuff(): # VIDEO ANALYSIS - GETS FACIAL COORDINATES AND DOES SPICY MAT
             try:
                 [nd1, led, red] = nr_approx(0,90,1,[en2, endist, en1, endist, eec, eedist])
                 [nd, led, red] = nr_approx(nd1-1,nd1,0.01,[en2, endist, en1, endist, eec, eedist])
-                #print("success")
                 rer = (red / ((e1h ** 2 + e1v ** 2 + 1)**0.5))
                 ler = (led / ((e2h ** 2 + e2v ** 2 + 1)**0.5))
                 prc = [(e1h * rer + e2h * ler)/2, (e1v * rer + e2v * ler)/2, (ler + rer)/2]
@@ -147,143 +146,72 @@ def camerastuff(): # VIDEO ANALYSIS - GETS FACIAL COORDINATES AND DOES SPICY MAT
                 changed = 1
             except:
                 pass
-            cv2.putText(image, "User Position: " + str(int(e1x)) + "," + str(int(e1y)) + "," + str(int(e1z)), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255))
+            cv2.putText(image, "User Position: " + str(round(e1x, 3)) + "," + str(round(e1y, 3)) + "," + str(round(e1z, 3)), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255))
             cv2.imshow('preview', image)
             cv2.waitKey(1)
             
 
-ktranslation = [0,-7,121]
-verticies1 = (
-    (-8, 5, 10),
-    (8, 5, 10),
-    (-8, -5, 10),
-    (8, -5, 10),
-    (-8, 5, 0),
-    (8, 5, 0),
-    (-8, -5, 0),
-    (8, -5, 0)    
-)
-
-edges1 = (
-    (0,1),
-    (1,3),
-    (0,2),
-    (2,3),
-    (4,5),
-    (5,7),
-    (4,6),
-    (6,7),
-    (0,4),
-    (1,5),
-    (2,6),
-    (3,7)
-    
-)
-
-def Cube():
-    glBegin(GL_LINES)
-    for edge in edges1:
-        for vertex in edge:
-            glColor3f(1, 0, 0)
-            glVertex3fv(verticies1[vertex])
-
-
-    glEnd()
+ktranslation = [0,0,0]
 
 def main():
-    global killed
-    global dilation
-    global scale
+    # viewport coordinates
+
+    top = 0
+    left = 0
+    right = 3840
+    bottom = 2400
+    
+
     pygame.init()
     display = (dispw,disph)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-    thisdw = dispw*scale
-    thisdh = disph*scale
-    glFrustum(-thisdw*0.01, thisdw*0.01, -thisdh*0.01, thisdh*0.01, 10*0.01, 50.0)
-    framenum = 0
-    thisloc = [0,0,20]
     model = OBJ('katana/katana.obj')
-    glTranslatef(0,0,-20)
+    object_length_irl = 20 # how long the object will appear, in inches.
+    object_length_gamespace = 259 # size in the game files.
+    world2game = object_length_gamespace/object_length_irl
+    monitor_height = screen_diagonal * disph / (dispw ** 2 + disph ** 2)**0.5
+    monitor_width = screen_diagonal * dispw / (dispw ** 2 + disph ** 2)**0.5
+    print("monitor width: " + str(monitor_width) + " inches.")
+    thisloc = [0,0,20*world2game]
+
     while True:
-        #print(ktranslation)
         glLoadIdentity()
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)       
 
-        distperinch = (((2*dispw*scale) ** 2 + (2*disph*scale)**2)**0.5)/screen_diagonal
-        if changed == 0:
-            glFrustum(-thisdw*0.01, thisdw*0.01, -thisdh*0.01, thisdh*0.01, 10*0.01, 50.0)
-        else:
-            thisloc = [(-e1x + camera_x_offset) * distperinch, distperinch*(e1y + camera_y_offset), (e1z + camera_z_offset)*distperinch] 
-            # thisloc is relative to the dimension of the laptop scren - assuming it is 16 units wide and 9 units tall
+        
 
-            left = -thisdw - thisloc[0]
-            right = thisdw - thisloc[0]
-            top = thisdh - thisloc[1]
-            bottom = -thisdh - thisloc[1]
-            clipping = thisloc[2] - 10
-            try:
-                glFrustum(left*0.01, right*0.01, bottom*0.01, top*0.01, clipping*0.01, 150.0)
-                glTranslatef(-thisloc[0],-thisloc[1],-thisloc[2])
-            except:
-                glFrustum(-thisdw*0.01, thisdw*0.01, -thisdh*0.01, thisdh*0.01, 10*0.01, 50.0)
-
+        thisloc = [(e1x + camera_x_offset)*world2game , -(e1y + camera_y_offset)* world2game, (e1z + camera_z_offset) * world2game] # eye's position rel. to the screen's position, in game space coordinates
+        left = thisloc[0] - (monitor_width/2 * world2game)
+        right = thisloc[0] + (monitor_width/2 * world2game)
+        top = thisloc[1] + (monitor_height/2 * world2game)
+        bottom = thisloc[1] - (monitor_height/2 * world2game)
+        near = thisloc[2]
+        glFrustum(left, right, bottom, top, near, 400.0)
+        glTranslatef(thisloc[0],thisloc[1],-thisloc[2]) # makes the eye location in the view matrix to the eye location in real life
+            
         
         keypress = pygame.key.get_pressed()
-        translation_mode = 0
-        if translation_mode == 0:
-            translated = 0
-            if keypress[pygame.K_w]:
-                ktranslation[2] -= 1
-                translated = 1
-            if keypress[pygame.K_s]:
-                ktranslation[2] += 1
-                translated = 1
-            if keypress[pygame.K_d]:
-                ktranslation[0] += 1
-                translated = 1
-            if keypress[pygame.K_a]:
-                ktranslation[0] -= 1
-                translated = 1
-            if keypress[pygame.K_z]:
-                ktranslation[1] -= 1
-                translated = 1
-            if keypress[pygame.K_x]:
-                ktranslation[1] += 1
-                translated = 1
-            if keypress[pygame.K_j]:
-                dilation *= 1.01
-                translated = 1
-            if keypress[pygame.K_l]:
-                dilation /= 1.01
-                translated = 1
-        else:
-            if keypress[pygame.K_w]:
-                translation[2] -= 0.1
-            if keypress[pygame.K_s]:
-                translation[2] += 0.1
-            if keypress[pygame.K_d]:
-                translation[0] += 0.02
-            if keypress[pygame.K_a]:
-                translation[0] -= 0.02
-            if keypress[pygame.K_z]:
-                translation[1] -= 0.1
-            if keypress[pygame.K_x]:
-                translation[1] += 0.1
-            if keypress[pygame.K_j]:
-                dilation *= 1.01
-            if keypress[pygame.K_l]:
-                dilation /= 1.01            
+        if keypress[pygame.K_w]:
+            ktranslation[2] -= 1
+        if keypress[pygame.K_s]:
+            ktranslation[2] += 1
+        if keypress[pygame.K_d]:
+            ktranslation[0] += 1
+        if keypress[pygame.K_a]:
+            ktranslation[0] -= 1
+        if keypress[pygame.K_z]:
+            ktranslation[1] -= 1
+        if keypress[pygame.K_x]:
+            ktranslation[1] += 1
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-        Cube()
-
-        glScalef(0.1 * dilation,0.1 * dilation,0.1 * dilation)
-        glTranslate(ktranslation[0], ktranslation[1],ktranslation[2])        
-        glPushMatrix()
+        glTranslate(ktranslation[0], ktranslation[1],ktranslation[2])# moves the object relative to the screen position        
+        
+        glPushMatrix() 
         model.render()
         glPopMatrix()
 
